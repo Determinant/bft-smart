94124db2e2cb6eb5e3c83de714a143259231b1a8
Determinant <ted.sybil@gmail.com>
2018-08-25 16:48:19 -0400

    ...

--- runscripts/run_demo.sh
+++ runscripts/run_demo.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-rep=({0..7})
+rep=({0..15})
 if [[ $# -gt 0 ]]; then
     rep=($@)
 fi
+rm -f config/currentView
 for i in "${rep[@]}"; do
     echo "starting replica $i"
     exec ./runscripts/smartrun.sh bftsmart.demo.counter.CounterServer "$i" > log"$i" 2>&1 &
